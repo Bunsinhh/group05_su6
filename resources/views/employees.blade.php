1609f6a9496c1f3808d2e18fa16edd85400b2328
Nam woon <namwoon@Nams-MacBook-Pro.local>
2025-03-27 14:25:56 +0700

    first commit

---
+++ resources/views/employees.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="vh-100 d-flex justify-content-center align-items-center">
+
+    <div class="text-center">
+        <h1 class="text-primary">Employee CRUD!</h1>
+        <p class="lead">This is a simple CRUD for employees.</p>
+        <a href="#" class="btn btn-success">Click Here!</a>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
